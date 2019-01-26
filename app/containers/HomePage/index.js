fcc9a2676bd18ab8027b95dcfa7307480ff7298d
Rajesh <rajeshkumaran.k@caratlane.com>
2019-01-26 15:12:29 +0530

    SSR integrated

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -1,25 +1,19 @@
-/*
+/**
+ *
  * HomePage
  *
- * This is the first thing users see of our App, at the '/' route
- *
- * NOTE: while this component should technically be a stateless functional
- * component (SFC), hot reloading does not currently support SFCs. If hot
- * reloading is not a necessity for you then you can refactor it and remove
- * the linting exception.
  */
-
 import React from 'react';
 
 /* eslint-disable react/prefer-stateless-function */
 export default class HomePage extends React.PureComponent {
   test = () => {
-    console.log('hello');
+    console.log('hello'); /* eslint-disable no-console */
   };
   render() {
     return (
       <div>
-        I am Homepage cool dude!!!!!!!!!!!!!!!!!
+        I am Homepage cool dude!!!!!!!!!!!!!
         <button onClick={this.test}>CLick</button>
       </div>
     );
